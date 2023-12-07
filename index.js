e1c1705c297cc93fc975bd912d88bb7d810c9009
Vatanyu2537 <118247571+Vatanyu2537@users.noreply.github.com>
2023-12-07 13:22:09 +0700

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;  // ใช้ PORT จาก environment หากมี หรือใช้ 3000 ถ้าไม่มี
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.send('Hello, World!');
 });
 
 app.listen(port, () => {
